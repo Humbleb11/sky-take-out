5e3114dbbc7c202942bbc3eee6d2761ce2ee7b04
Humbleb11 <1084283706@qq.com>
2024-07-01 19:48:16 +0800

    删除菜品业务开发

--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -1,4 +1,4 @@
-package com.sky.service.impl;
+package com.sky.service;
 
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
-import com.sky.context.BaseContext;
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
@@ -13,12 +12,12 @@ import com.sky.mapper.CategoryMapper;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.result.PageResult;
-import com.sky.service.impl.CategoryService;
+import com.sky.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
+
 import java.util.List;
 
 /**
@@ -47,11 +46,13 @@ public class CategoryServiceImpl implements CategoryService {
         //分类状态默认为禁用状态0
         category.setStatus(StatusConstant.DISABLE);
 
-        //设置创建时间、修改时间、创建人、修改人
+        /*
+        设置创建时间、修改时间、创建人、修改人
         category.setCreateTime(LocalDateTime.now());
-        category.setUpdateTime(LocalDateTime.now());
+         category.setUpdateTime(LocalDateTime.now());
         category.setCreateUser(BaseContext.getCurrentId());
         category.setUpdateUser(BaseContext.getCurrentId());
+        */
 
         categoryMapper.insert(category);
     }
@@ -99,9 +100,11 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = new Category();
         BeanUtils.copyProperties(categoryDTO,category);
 
-        //设置修改时间、修改人
+        /*
+       设置修改时间、修改人
         category.setUpdateTime(LocalDateTime.now());
         category.setUpdateUser(BaseContext.getCurrentId());
+       */
 
         categoryMapper.update(category);
     }
@@ -115,8 +118,8 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = Category.builder()
                 .id(id)
                 .status(status)
-                .updateTime(LocalDateTime.now())
-                .updateUser(BaseContext.getCurrentId())
+               // .updateTime(LocalDateTime.now())
+               // .updateUser(BaseContext.getCurrentId())
                 .build();
         categoryMapper.update(category);
     }
