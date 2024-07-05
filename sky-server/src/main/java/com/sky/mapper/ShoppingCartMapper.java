3171b302a2e429f4aab6ffd56752ddd4cb3621b0
Humbleb11 <1084283706@qq.com>
2024-07-06 02:19:08 +0800

    购物车功能代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,32 @@
+package com.sky.mapper;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    // 动态查询购物车列表
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+    //根据Id修改商品数量
+    @Update("update shopping_cart set number =#{number} where id=#{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+    //插入购物车数据
+    @Insert("insert into shopping_cart(name,  user_id, dish_id, setmeal_id, dish_flavor,number, amount, image,create_time) "+
+            "values (#{name},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{image},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+    //清空购物车
+    @Delete("delete from shopping_cart where user_id=#{userId}")
+    void deleteByUserId(Long userId);
+    /**
+     * 根据id删除购物车数据
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
+}
